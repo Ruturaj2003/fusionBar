2d9b67439b4544d484e6c3ee06e3f0728e30737d
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-23 22:29:28 +0530

    Update Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,31 @@
+import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/not-found.svg';
 const Error = () => {
+  const error = useRouteError();
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="Page Not Found" />
+
+          <h3>
+            Oops! The page you are looking for might be in another castle.
+          </h3>
+          <br />
+          <h2>
+            <Link to="/">Go Back Home :D</Link>
+          </h2>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div className="not-found-container">
-      <h1 className="e1">404 - Not Found</h1>
-      <br></br>
-      <p className="p1">
-        Oops! The page you are looking for might be in another castle.
-      </p>
+    <div>
+      <h2>
+        Error 503: The server is doing yoga. It'll be back in a zen state
+        shortly
+      </h2>
     </div>
   );
 };
